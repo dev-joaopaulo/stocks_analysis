ef73946b84e1ea8765aa0a852ed9a3e84bbd5c86
devonfire <joaopspp@gmail.com>
2023-12-30 09:51:50 -0300

    added position sizing methods for future use

---
+++ position_sizing.py
@@ -0,0 +1,15 @@
+import math
+
+
+def get_unit_risk(entry, stop):
+    return entry - stop
+
+
+def get_number_stocks_based_on_risk(unit_risk, max_risk):
+    return math.ceil(max_risk / unit_risk)
+
+
+def get_risk_target_based_on_number_stocks(unit_risk, num_stocks, profit_target):
+    return profit_target/(unit_risk * num_stocks)
+
+
